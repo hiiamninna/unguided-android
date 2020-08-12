3805420b915ed13b95de5b695c95f2fc7b5fdb5b
ninnanovila <170709235@students.uajy.ac.id>
2020-08-12 12:51:00 +0700

    done for ugd-final

--- app/src/main/java/com/ninnanovila/menulayout/MainActivity.java
+++ app/src/main/java/com/ninnanovila/menulayout/MainActivity.java
@@ -3,6 +3,10 @@ package com.ninnanovila.menulayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextInputEditText fullNameInput = findViewById(R.id.fullname_input);
+        Button submitBtn = findViewById(R.id.submit_button);
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //INISIALISASI
+                FoodFragment foodFragment = new FoodFragment();
+                Bundle data = new Bundle();
+
+                //PUT STRING
+                String message = fullNameInput.getText().toString();
+                data.putString(FoodFragment.FULL_NAME, message);
+
+                //REPLACE LAYOUT
+                foodFragment.setArguments(data);
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, foodFragment)
+                        .commit();
+            }
+        });
     }
 }
